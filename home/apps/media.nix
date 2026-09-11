79b6ef8ae079b45d88c3f2d1bbde17e6ffc813ac
IberAI <ilteber.dover@gmail.com>
2026-09-11 16:01:30 -0400

    Added newsboat and some urls

--- home/apps/media.nix
+++ home/apps/media.nix
@@ -4,7 +4,6 @@
   ];
 
   home.packages = with pkgs; [
-    newsboat
     mpv
     yt-dlp
   ];
---
+++ home/apps/newsboat.nix
@@ -0,0 +1,62 @@
+{pkgs, ...}: {
+  programs.newsboat = {
+    enable = true;
+    package = pkgs.newsboat;
+
+    # Normal links open in your browser.
+    browser = "$BROWSER";
+
+    autoReload = false;
+    reloadThreads = 5;
+
+    # 0 = unlimited articles per feed.
+    maxItems = 0;
+
+    autoFetchArticles = {
+      enable = true;
+      onCalendar = "hourly";
+    };
+
+    autoVacuum = {
+      enable = true;
+      onCalendar = "weekly";
+    };
+
+    urls = [
+      {
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCG1ll_JKswdPuwc2xcMINrQ";
+        title = "John Kleinbauer";
+        tags = [
+          "youtube"
+          "radio"
+          "electronics"
+        ];
+      }
+
+      {
+        url = "http://makingembeddedsystems.libsyn.com/rss";
+        title = "Making Embedded Systems";
+        tags = [
+          "podcast"
+          "embedded"
+        ];
+      }
+
+      {
+        url = "https://api.substack.com/feed/podcast/865289/s/339273.rss";
+        title = "Casey Muratori";
+        tags = [
+          "podcast"
+          "software"
+        ];
+      }
+    ];
+
+    queries = {};
+
+    extraConfig = ''
+      # Press 'p' to play the current item's first media enclosure with mpv.
+      macro p set browser "${pkgs.mpv}/bin/mpv --player-operation-mode=pseudo-gui -- %u &"; one; set browser "$BROWSER"
+    '';
+  };
+}
