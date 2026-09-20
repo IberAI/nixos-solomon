94d36f7192902c2e8bd7f260f7176b88582fbf3f
IberAI <ilteber.dover@gmail.com>
2026-09-20 11:07:43 -0400

    sioyek was not fixed lol its not i hope

--- home/apps/sioyek.nix
+++ home/apps/sioyek.nix
@@ -5,20 +5,11 @@
 }: {
   programs.sioyek = {
     enable = true;
-
-    package = pkgs.symlinkJoin {
-      name = "sioyek-nvidia";
-      paths = [pkgs.sioyek];
-
-      nativeBuildInputs = [pkgs.makeWrapper];
-
-      postBuild = ''
-        wrapProgram $out/bin/sioyek \
-          --set QT_QPA_PLATFORM xcb \
-          --set __GLX_VENDOR_LIBRARY_NAME nvidia
-      '';
-    };
-
+    package = pkgs.writeShellScriptBin "sioyek" ''
+      export QT_QPA_PLATFORM=xcb
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      exec ${pkgs.sioyek}/bin/sioyek "$@"
+    '';
     # Optional: preferences/bindings if you want to manage them declaratively.
     # config = {
     #   ui_font_size = "18";
