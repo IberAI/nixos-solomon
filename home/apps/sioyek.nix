5db4bb4855de0a0f0cb7933836921cfe80a479a5
IberAI <ilteber.dover@gmail.com>
2026-09-20 10:56:10 -0400

    sioyek nvidia api issue solved

--- home/apps/sioyek.nix
+++ home/apps/sioyek.nix
@@ -5,20 +5,31 @@
 }: {
   programs.sioyek = {
     enable = true;
-    package = pkgs.sioyek;
+
+    package = pkgs.symlinkJoin {
+      name = "sioyek-nvidia";
+      paths = [pkgs.sioyek];
+
+      nativeBuildInputs = [pkgs.makeWrapper];
+
+      postBuild = ''
+        wrapProgram $out/bin/sioyek \
+          --set QT_QPA_PLATFORM xcb \
+          --set __GLX_VENDOR_LIBRARY_NAME nvidia
+      '';
+    };
 
     # Optional: preferences/bindings if you want to manage them declaratively.
     # config = {
-    #   "ui_font_size" = "18";
+    #   ui_font_size = "18";
     # };
     #
     # bindings = {
-    #   "j" = "down";
-    #   "k" = "up";
+    #   j = "down";
+    #   k = "up";
     # };
   };
 
-  # Optional: make sure config dir exists (safe)
   home.activation.createSioyekDir = lib.hm.dag.entryBefore ["writeBoundary"] (
     builtins.readFile ../../scripts/create-sioyek-dir.sh
   );
